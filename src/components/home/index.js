352b2a1a1f1ff33b1c0e7b86c172031c3873bf5e
Caio Martins <caio_martins02@yahoo.com.br>
2020-12-02 20:44:53 -0300

    iniciando projeto ToDo App com o Cicatriz

---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header>
+      <h1>Hello React World</h1>
+      </Header>
+    </div>
+  )
+};
+
+export default Home;
